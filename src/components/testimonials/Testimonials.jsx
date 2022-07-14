1021b6acc2dcf983553093df9f38348d9d668756
tonny187 <tonyoanthony@gmail.com>
2022-07-13 19:39:54 -0500

    falta un blog y el header

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -18,17 +18,20 @@ const data = [
   {
     image: explorer,
     title: 'La despedida de internet explorer',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perspiciatis ea aliquid tempora quis vitae magnam doloribus. Dolor, tempora iure dicta soluta eius eum consequatur reiciendis nulla, molestiae recusandae exercitationem!'
+    review: 'Que pensamiento o sentimiento se te viene a la mente cuando ves este icono? Quizás nostalgia, algún recuerdo emocionante, antipatía, pero lo más probable es que lo veas y te de enojo, IIRAAA, RABIAAAAAA!!!! Y es que la lentitud...',
+    link: 'https://tonnys-lab.blogspot.com/2022/07/la-despedida-de-internet-explorer.html'
   },
   {
     image: next,
     title: 'Mi experiencia con Nextjs',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perspiciatis ea aliquid tempora quis vitae magnam doloribus. Dolor, tempora iure dicta soluta eius eum consequatur reiciendis nulla, molestiae recusandae exercitationem!'
+    review: 'Si de Tecnologías Frontend hablamos, Nextjs debe ser el pináculo, la cúspide, la punta de la pirámide en los sitios web, probablemente te preguntes, si React es un framework (o librería) de javascript, para que necesitamos otro framework ahora para react?',
+    link: 'https://tonnys-lab.blogspot.com/2022/07/mi-experiencia-con-nextjs.html'
   },
   {
     image: solidity,
     title: 'El futuro de Solidity',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perspiciatis ea aliquid tempora quis vitae magnam doloribus. Dolor, tempora iure dicta soluta eius eum consequatur reiciendis nulla, molestiae recusandae exercitationem!'
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perspiciatis ea aliquid tempora quis vitae magnam doloribus. Dolor, tempora iure dicta soluta eius eum consequatur reiciendis nulla, molestiae recusandae exercitationem!',
+    link: 'https://tonnys-lab.blogspot.com/2022/07/la-despedida-de-internet-explorer.html'
   }
 ]
 
@@ -47,7 +50,7 @@ const Testimonials = () => {
 
       >
         {
-          data.map(({image, title, review}, index) => {
+          data.map(({image, title, review, link}, index) => {
             return (
               <SwiperSlide key={index} className='testimonial'>
                 <div className="client_avatar">
@@ -57,6 +60,7 @@ const Testimonials = () => {
                   <small className='client_review'>
                     {review}
                   </small>
+                  <a href={link} target="_blank" rel='noreferrer' className='btn_blog'>Visit</a>
               </SwiperSlide>
             )
           } )
