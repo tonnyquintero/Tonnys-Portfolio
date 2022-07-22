2e57a40817f52ee91ed5edaf8083a56df6e03dbe
tonny187 <tonyoanthony@gmail.com>
2022-07-21 19:12:03 -0500

    integrando CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import TonnyCv from '../../assets/tonnysCv.pdf'
 
 const CTA = () => {
 
@@ -8,7 +9,7 @@ const CTA = () => {
 
   return (
     <div className='cta'>
-        <a href='#contact' download className='btn'>{t('cta.download')}</a>
+        <a href={TonnyCv} download className='btn'>{t('cta.download')}</a>
         <a href='#contact' className='btn btn-primary'>{t('cta.letsTalk')}</a>
     </div>
   )
